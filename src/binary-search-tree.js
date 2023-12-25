ee5f084a76312f432adeb6e5f8485770f80139c1
aauroraaborealis <imaginepeach@mail.ru>
2023-12-25 21:41:55 +0300

    init: start task & feat: made a three task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,138 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+  constructor(data) {
+    this.data = data; // node value
+    this.leftChild = null;  // leftChild node child reference
+    this.rightChild = null; // rightChild node child reference
+  }
+ }
+
 class BinarySearchTree {
 
+  constructor(){
+    this._root = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!data) throw new Error("Data must be provided");
+    const newNode = new Node(data);
+    if (!this._root){
+      this._root = newNode;
+    } else {
+      this.insertNode(this._root, newNode)
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.leftChild === null) {
+        node.leftChild = newNode;
+      } else {
+        this.insertNode(node.leftChild, newNode);
+      }
+    } else {
+      if (node.rightChild === null) {
+        node.rightChild = newNode;
+      } else {
+        this.insertNode(node.rightChild, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (!data) throw new Error("Data must be provided");
+    return this.find(data) !== null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (!data) throw new Error("Data must be provided");
+    return this.search(this._root, data);
   }
+
+  search(node, data) {
+    switch (true) {
+      case node === null:
+        return null;
+      case data < node.data:
+        return this.search(node.leftChild, data);
+      case data > node.data:
+        return this.search(node.rightChild, data);
+      default:
+        return node;
+    }
+   }
+
+  remove(data) {
+    if (!data) throw new Error("Data must be provided");
+    if (this.has(data)){
+      this.removeNode(this._root, data);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+removeNode(node, data) {
+ switch (true) {
+  case node === null:
+    return null;
+  case data < node.data:
+    node.leftChild = this.removeNode(node.leftChild, data);
+    return node;
+  case data > node.data:
+    node.rightChild = this.removeNode(node.rightChild, data);
+    return node;
+  default:
+    if (node.leftChild === null && node.rightChild === null) {
+      node = null;
+      return node;
+    }
+    if (node.leftChild === null) {
+      node = node.rightChild;
+      return node;
+    } else if(node.rightChild === null) {
+      node = node.leftChild;
+      return node;
+    }
+    let newNode = this.minNode(node.rightChild);
+    node.data = newNode.data;
+    node.rightChild = this.removeNode(node.rightChild, newNode.data);
+    return node;
+ }
+}
+
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._root) throw new Error("Tree is empty");
+    let minimalNode = this.minNode(this._root);
+    return minimalNode.data;
+  }
+
+  minNode(node) {
+    if (node.leftChild === null)
+        return node;
+    else
+        return this.minNode(node.leftChild);
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this._root) throw new Error("Tree is empty");
+    let maximumNode = this.maxNode(this._root);
+    return maximumNode.data;
+  }
+
+  maxNode(node) {
+    if (node.rightChild === null)
+        return node;
+    else
+        return this.maxNode(node.rightChild);
   }
 }
 
