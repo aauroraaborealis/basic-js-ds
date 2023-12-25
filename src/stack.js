7b4244c53f021c9b8b8a2afb80010f0b7636b96e
aauroraaborealis <imaginepeach@mail.ru>
2023-12-25 22:19:44 +0300

    feat: made all tasks

--- src/stack.js
+++ src/stack.js
@@ -23,13 +23,17 @@ class Stack {
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+    return this.stack[this.stack.length - 1];
   }
 }
 
